f750162748e8be713ca5fcd87dd5117f402233ed
Profile <73017153+Dmittur@users.noreply.github.com>
2021-11-29 21:53:50 +0700

    Update shared-node.sh

--- shared-node.sh
+++ shared-node.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 sudo ufw disable
+sudo apt update
+sudo apt-get update && apt-get upgrade -y
+sudo apt full-upgrade -y
+#Files
+sudo echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
+#swap file 2 Gb
+sudo swapon --show
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
+sudo echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+cat /proc/sys/vm/swappiness
+sudo echo 'vm.swappiness=25' >> /etc/sysctl.conf
+#Time
+sudo apt install ntpsec
+sudo service ntpsec restart
+sudo iptables -A INPUT -p tcp -m tcp --dport 22 -m state --state NEW -m hashlimit --hashlimit 1/hour --hashlimit-burst 2 --hashlimit-mode srcip --hashlimit-name SSH --hashlimit-htable-expire 60000 -j ACCEPT
+sudo iptables -A INPUT -p tcp -m tcp --dport 22 --tcp-flags SYN,RST,ACK SYN -j DROP
+sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
+sudo apt install iptables-persistent -y
+sudo service netfilter-persistent save
+sudo iptables -L -n --line-numbers
+sudo apt-get install zip unzip screen wget -y
 if [ -d "/root/idena-node-proxy" ]; then
 echo "idena-node-proxy already installed"
 else
@@ -69,10 +94,14 @@ IDENA_KEY=\"123\"
 PORT=80" > .env'
 #GOD_API_KEY=\"test\"
 #REMOTE_KEYS_ENABLED=0
-
 npm install
 sed -i 's/stdout/file/g' config_default.json
 npm start
 pm2 startup
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw enable
+sudo ufw reload
+ulimit -n 200000
 sudo reboot
 fi
